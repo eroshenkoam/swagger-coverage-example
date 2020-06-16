a235e7a556cb43d44e9395955f3feac28464f7ad
Artem Eroshenko <eroshenkoam@qameta.io>
2020-06-16 18:11:39 +0300

    first draft

---
+++ src/test/java/io/eroshenkoam/swagger/SwaggerCoverageTest.java
@@ -0,0 +1,23 @@
+package io.eroshenkoam.swagger;
+
+import org.junit.jupiter.api.Test;
+
+import static io.eroshenkoam.swagger.RestAssuredUtil.defaultSpec;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SwaggerCoverageTest {
+
+    @Test
+    public void testCoverage() {
+        // @formatter:off
+        given().spec(defaultSpec())
+        .when()
+            .get("/pet/{id}", 1)
+        .then()
+            .statusCode(404);
+        // @formatter:on
+
+    }
+
+}
